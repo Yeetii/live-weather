87900828d0a09e914576243c9e4635801232e57b
Erik Magnusson <erik.magnusson@forefront.se>
2024-11-28 17:42:38 +0100

    Load temp and wind from skistar

---
+++ backend/lib/firestore.go
@@ -0,0 +1,95 @@
+package lib
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"os"
+
+	firebase "firebase.google.com/go"
+	geojson "github.com/paulmach/go.geojson"
+	"google.golang.org/api/option"
+)
+
+type Observation struct {
+	Id               *string  `json:"id"`
+	Name             *string  `json:"name"`
+	Latitude         *float64 `json:"latitude"`
+	Longitude        *float64 `json:"longitude"`
+	Elevation        *float64 `json:"elevation"`
+	TemperatureC     *float64 `json:"temperature_c"`
+	WindSpeedMs      *float64 `json:"windSpeed_ms"`
+	WindDirectionDeg *float64 `json:"windDirection_deg"`
+	WindGustSpeedMs  *float64 `json:"windGustSpeed_ms"`
+	HumidityPercent  *float64 `json:"humidity_percent"`
+	NewSnow24hCm     *float64 `json:"newSnow24h_cm"`
+	SnowDepthCm      *float64 `json:"snowDepth_cm"`
+	VisibilityM      *float64 `json:"visibility_m"`
+}
+
+func UploadObservationsToFirestore(observations []Observation) error {
+	ctx := context.Background()
+	var opts []option.ClientOption
+	if _, err := os.Stat("service-account.json"); err == nil {
+		opts = append(opts, option.WithCredentialsFile("service-account.json"))
+	}
+	conf := &firebase.Config{
+		DatabaseURL: "https://live-weather-eefc5.firebaseio.com",
+	}
+	app, err := firebase.NewApp(ctx, conf, opts...)
+	if err != nil {
+		log.Fatalf("error initializing app: %v", err)
+	}
+
+	firestoreClient, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalf("error initializing Firestore client: %v", err)
+	}
+	defer firestoreClient.Close()
+
+	var features []geojson.Feature
+	for _, observation := range observations {
+		feature := geojson.NewPointFeature([]float64{*observation.Longitude, *observation.Latitude})
+		feature.ID = *observation.Id
+		feature.Properties = map[string]interface{}{
+			"name":              observation.Name,
+			"elevation":         observation.Elevation,
+			"temperature_c":     observation.TemperatureC,
+			"windSpeed_ms":      observation.WindSpeedMs,
+			"windDirection_deg": observation.WindDirectionDeg,
+			"windGustSpeed_ms":  observation.WindGustSpeedMs,
+			"humidity_percent":  observation.HumidityPercent,
+			"newSnow24h_cm":     nil,
+			"snowDepth_cm":      observation.SnowDepthCm,
+			"visibility_m":      observation.VisibilityM,
+		}
+		features = append(features, *feature)
+	}
+
+	for _, feature := range features {
+		id, ok := feature.ID.(string)
+		if !ok {
+			log.Fatal("feature.ID is not a string")
+		}
+
+		geoJsonBytes, marshalErr := feature.MarshalJSON()
+		if marshalErr != nil {
+			log.Printf("Failed to marshal feature: %v", marshalErr)
+			return marshalErr
+		}
+
+		var geoJsonMap map[string]interface{}
+		unmarshalErr := json.Unmarshal(geoJsonBytes, &geoJsonMap)
+		if unmarshalErr != nil {
+			log.Printf("Failed to unmarshal JSON into map: %v", unmarshalErr)
+			return unmarshalErr
+		}
+
+		_, err := firestoreClient.Collection("weatherObservations").Doc(id).Set(ctx, geoJsonMap)
+		if err != nil {
+			log.Printf("Failed to store document: %v", err)
+			return err
+		}
+	}
+	return nil
+}
---
+++ backend/updateSkistarWeather.go
@@ -0,0 +1,135 @@
+package functions
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+	"strconv"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/Yeetii/live-weather/lib"
+)
+
+type SkistarWeatherInput struct {
+	Id             string
+	Url            string
+	TopLocation    []float64
+	BottomLocation []float64
+}
+
+func init() {
+	functions.HTTP("updateSkistarWeather", updateSkistarWeather)
+}
+
+func updateSkistarWeather(w http.ResponseWriter, r *http.Request) {
+	inputs := []SkistarWeatherInput{
+		{Id: "areby", Url: "https://www.skistar.com/Lpv/Forecast?lang=sv&area=areby", TopLocation: nil, BottomLocation: []float64{63.403513916879106, 13.059243790348805}},
+		{Id: "hogzon", Url: "https://www.skistar.com/Lpv/Forecast?lang=sv&area=hogzon", TopLocation: []float64{63.42746531861163, 13.07798918790169}, BottomLocation: nil},
+		{Id: "bjornen", Url: "https://www.skistar.com/Lpv/Forecast?lang=sv&area=bjornen", TopLocation: []float64{63.40397330198576, 13.112439722480248}, BottomLocation: []float64{63.39058903591519, 13.124520371380962}},
+		{Id: "duved", Url: "https://www.skistar.com/Lpv/Forecast?lang=sv&area=duved", TopLocation: []float64{63.40925052213198, 12.933974437408123}, BottomLocation: []float64{63.39653432268454, 12.924465636198212}},
+	}
+
+	var observations []lib.Observation
+
+	for _, input := range inputs {
+		var weather = scrapeCurrentWeather(input.Url)
+		log.Println(input.Id)
+		log.Println(weather)
+		if input.TopLocation != nil {
+			id := "skistar-" + input.Id + "-top"
+			observation := lib.Observation{Id: &id, Latitude: &input.TopLocation[0], Longitude: &input.TopLocation[1], TemperatureC: &weather.TemperatureTop, WindSpeedMs: &weather.WindSpeedTop, WindGustSpeedMs: &weather.GustWindpeedTop}
+			observations = append(observations, observation)
+		}
+		if input.BottomLocation != nil {
+			id := "skistar-" + input.Id + "-bottom"
+			observation := lib.Observation{Id: &id, Latitude: &input.BottomLocation[0], Longitude: &input.BottomLocation[1], TemperatureC: &weather.TemperatureBottom, WindSpeedMs: &weather.GustWindspeedBottom, WindGustSpeedMs: &weather.GustWindspeedBottom}
+			observations = append(observations, observation)
+		}
+	}
+	lib.UploadObservationsToFirestore(observations)
+}
+
+type weatherMeasurement struct {
+	TemperatureTop      float64
+	TemperatureBottom   float64
+	WindSpeedTop        float64
+	WindSpeedBottom     float64
+	GustWindpeedTop     float64
+	GustWindspeedBottom float64
+}
+
+func scrapeCurrentWeather(url string) weatherMeasurement {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Fatalf("Failed to fetch the webpage: %s", res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tempFields := doc.Find(".lpv-info-weather__text")
+
+	topTempElement := tempFields.First()
+	bottomTempElement := tempFields.Eq(1)
+
+	topTemp, err := extractFloat(topTempElement)
+	if err != nil {
+		log.Println(err)
+	}
+	bottomTemp, err := extractFloat(bottomTempElement)
+	if err != nil {
+		log.Println(err)
+	}
+
+	windContainers := doc.Find(".lpv-info-list__value")
+
+	topWindContainer := windContainers.First()
+
+	topWindSpeed, err := extractFloat(topWindContainer.Children().First())
+	if err != nil {
+		log.Println(err)
+	}
+	gustWindpeedTop, err := extractFloat(topWindContainer.Children().Eq(1))
+	if err != nil {
+		log.Println(err)
+	}
+
+	bottomWindContainer := windContainers.Eq(1)
+
+	bottomWindSpeed, err := extractFloat(bottomWindContainer.Children().First())
+	if err != nil {
+		log.Println(err)
+	}
+	gustWindspeedBottom, err := extractFloat(bottomWindContainer.Children().Eq(1))
+	if err != nil {
+		log.Println(err)
+	}
+
+	return weatherMeasurement{
+		TemperatureTop:      topTemp,
+		TemperatureBottom:   bottomTemp,
+		WindSpeedTop:        topWindSpeed,
+		GustWindpeedTop:     gustWindpeedTop,
+		WindSpeedBottom:     bottomWindSpeed,
+		GustWindspeedBottom: gustWindspeedBottom,
+	}
+}
+
+func extractFloat(element *goquery.Selection) (float64, error) {
+	text := element.Text()
+	re := regexp.MustCompile(`[-]?\d+\.?\d*`)
+	match := re.FindString(text)
+	if match == "" {
+		return 0, error(fmt.Errorf("no float found in string: %s", text))
+	}
+	return strconv.ParseFloat(match, 64)
+}
